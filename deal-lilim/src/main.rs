348784655086988aa38f01e765123cd9172cdc42
NeekTwoThousand <craftmallus@gmail.com>
2024-02-20 13:23:24 +0100

    print redact() Err to stderr

--- deal-lilim/src/main.rs
+++ deal-lilim/src/main.rs
@@ -28,7 +28,10 @@ struct User {
 async fn delete_message(room: Room, res: send_message_event::v3::Response) {
 	let event_id = res.event_id;
 	sleep(Duration::new(3600, 0)).await;
-	let _ = room.redact(&event_id, None, None).await;
+	let res = room.redact(&event_id, None, None).await;
+	if let Err(res) = res {
+		eprintln!("redact:\n{:#?}", res);
+	}
 }
 
 fn read_stream(mut stream: UnixStream) -> String {
