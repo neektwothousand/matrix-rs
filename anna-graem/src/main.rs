0f54c1af9dc373222339ccd954d35f28f836a32f
NeekTwoThousand <craftmallus@gmail.com>
2024-09-24 00:43:48 +0200

    exit if can't get matrix client user id

--- anna-graem/src/main.rs
+++ anna-graem/src/main.rs
@@ -103,6 +103,8 @@ async fn main() -> anyhow::Result<()> {
 	});
 	if matrix_client.user_id().is_some() {
 		log::info!("matrix client logged in");
+	} else {
+		anyhow::bail!("matrix client can't log in");
 	}
 	log::info!("tg dispatched");
 	loop {
