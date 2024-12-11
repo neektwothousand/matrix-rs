fe72ec572273e8b3ee36eea4992b5b42523723cc
NeekTwoThousand <craftmallus@gmail.com>
2024-12-11 01:10:35 +0100

    telegram stickers don't support captions

--- lib/tg-matrix-bridge/src/bridge_utils.rs
+++ lib/tg-matrix-bridge/src/bridge_utils.rs
@@ -156,16 +156,7 @@ pub async fn get_to_tg_data<'a>(
 		MessageType::Image(i) => {
 			let ec = ImageMessageEventContent::new(i.body.clone(), i.source.clone());
 			tg_data.message = get_event_content_vec(ec, &client).await?;
-			let is_sticker = i
-				.info
-				.as_ref()
-				.and_then(|info| info.mimetype.as_ref())
-				.is_some_and(|mimetype| mimetype == "image/webp");
-			tg_data.tg_message_kind = Some(if is_sticker {
-				TgMessageKind::Sticker
-			} else {
-				TgMessageKind::Photo
-			});
+			tg_data.tg_message_kind = Some(TgMessageKind::Photo);
 			tg_data.caption = Some(i.body.clone());
 		}
 		MessageType::Video(v) => {
